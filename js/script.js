b82f37d3f0abc039575510a66989e84725f9f052
rustychaiNZ <rusljohn@gmail.com>
2020-02-20 23:34:59 +1300

    map displaying with one marker

--- js/script.js
+++ js/script.js
@@ -6,6 +6,10 @@ $(document).ready(function(){
 	// var myKey = JSON.parse(apiKey); // Convert JSON data into js object
 	// console.log(myKey[0]);
 	// myKey = myKey[0].key;
+
+	var myKey = JSON.parse(apiKey);
+	console.log(myKey[0]);
+	myKey = myKey[0].key;
 	
 	// Dynamically creating the script element
 	// var script = document.createElement('script');
@@ -14,20 +18,10 @@ $(document).ready(function(){
 	// Appending to the body of index.html
 	// document.getElementsByTagName('body')[0].appendChild(script);
 
-	var myKey = JSON.parse(apiKey); // Convert JSON data into js object
-
-	// Dynamically creating the script element
-	var script = document.createElement('script');
-	// Giving the src attribute to the google plug in from external json file
-	script.src = 'https://maps.googleapis.com/maps/api/js?key=' + myKey[0].key + '&callback=initMap';
-	// Appending to the body of index.html
-	document.getElementsByTagName('body')[0].appendChild(script);
-
-
 	var i;
 	var k;
-	var long;
-	var lat;
+	var obj = {};
+	var markers = [];
 	// varibale for storing the users inputted end point
 	var endPoint;
 
@@ -39,40 +33,62 @@ $(document).ready(function(){
 		success : function(data){
 			console.log(data);
 			var markers = [];
+
 			// Loops through the object with the arrays
 			for(i = 0; i < data.features.length; i++){
-				// Loops through coordinates
-				for(k = 0; k < data.features[i].geometry.coordinates.length; k++){
-					long = data.features[i].geometry.coordinates[0];
-					lat = data.features[i].geometry.coordinates[1];
-					console.log('latitude ' + lat + ' longditude ' + long);
-				}
+				// Loops through coordinates and displays them in the console
+				// for(k = 0; k < data.features[i].geometry.coordinates.length; k++){
+					// console.log(data.features[i].geometry.coordinates[k]);
+					// long = data.features[i].geometry.coordinates[0];
+					// latt = data.features[i].geometry.coordinates[1];
+					// console.log('latitude ' + latt + ' longditude ' + long);
+				// }
+
+				// stores coordinates in an object
+				obj.lat = JSON.parse(data.features[i].geometry.coordinates[1]);
+				obj.lng = JSON.parse(data.features[i].geometry.coordinates[0]);
+
+				// Pushes long and lattitude into the obj object
+				markers.push(obj);
 			}
+			console.log(markers);
 			initMap(markers);
 		} , 
 		error : function(){
 			alert('You done goofed somewhere in your code');
 		}
+
 	});
-});
+	// Ajax ends
 
-var map;
-function initMap(allMarkers) {
-	var marker = [];
-	var Wellington = {lat: -41.2865, lng: 174.7762};
-	// Creating the new map object
-	var map = new google.maps.Map(document.getElementById('map'), {
-		zoom: 6,
-		center: Wellington  
-	});
-	// var myIcon = {
-	// 	url : 'http://maps.google.cim/mapfiles/kml/shapes/sailing.png' , 
-	// 	scaledSize: new google.maps.Size(50, 50)
-	// };
+	//dynamically creating script tag and appending to the html body including the apikey
+	var script = document.createElement('script');
+	script.src = 'https://maps.googleapis.com/maps/api/js?key='+ myKey ;
+	document.getElementsByTagName('body')[0].appendChild(script);
 
+	// Adds map markers based on cooridinates
+	function initMap(allMarkers) {
+		console.log(allMarkers);
+		var marker = [];
+		// The center point of the map, Wellington
+		var Wellington = {lat: -41.2865, lng: 174.7762};
+		// Creating the new map object
+		var map = new google.maps.Map(
+			document.getElementById('map'), {zoom: 6, center: Wellington});
+		// var i;
+		var myIcon = {
+			url : 'http://maps.google.com/mapfiles/kml/shapes/sailing.png' , 
+			scaledSize: new google.maps.Size(50, 50)
+		};
+	
+		for(i = 0; i < allMarkers.length; i++){
+			var latLng = {lat:allMarkers[i].lat , lng:allMarkers[i].lng};
+			
+			marker = new google.maps.Marker({
+				position: latLng,
+				map: map
+			});
+		}
+	}
+}); // Document ready ends
 
-
-	//Associate the styled map with the MapTypeId and set it to display.
-    // map.mapTypes.set('styled_map', styledMapType);
-    // map.setMapTypeId('styled_map');
-}
