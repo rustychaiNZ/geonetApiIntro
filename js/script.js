1a32c4b6b71bb65013fc5a0912b29ee24a47eb0a
Studio J Designs <studio-j-designs@Dont-Ask.local>
2020-02-27 12:53:14 +1300

    Added new js

--- js/script.js
+++ js/script.js
@@ -1,93 +1,70 @@
-// Retrieving Geonet information
+console.log('blog');
+
 $(document).ready(function(){
-	
-	// accessing apiKey from config.json
-	// apiKey = '[{"key" : "yourKeyGoesHere"}]';
-	// var myKey = JSON.parse(apiKey); // Convert JSON data into js object
-	// console.log(myKey[0]);
-	// myKey = myKey[0].key;
 
-	var myKey = JSON.parse(apiKey);
-	console.log(myKey[0]);
-	myKey = myKey[0].key;
-	
-	// Dynamically creating the script element
-	// var script = document.createElement('script');
-	// Giving the src attribute to the google plug in from external json file
-	// script.src = 'https://maps.googleapis.com/maps/api/js?key=' + myKey + '&callback=initMap';
-	// Appending to the body of index.html
-	// document.getElementsByTagName('body')[0].appendChild(script);
+  var lat = 175;
+  var lng = 40;
 
-	var i;
-	var markers = [];
-	// varibale for storing the users inputted end point
-	var endPoint;
+  var myKey = JSON.parse(apiKey); //convert JSON data into js object
+   console.log (myKey[0].key);
 
-	// ajax file being retrieved from geonet
-	$.ajax({
-		url : 'https://api.geonet.org.nz/intensity?type=reported' ,
-		type : 'GET' , 
-		dataType : 'json' , 
-		success : function(data){
-			console.log(data);
-			var markers = [];
-			var i;
-			// Loops through the object with the arrays
-			for(i = 0; i < data.features.length; i++){
-				// Loops through coordinates and displays them in the console
-				// for(k = 0; k < data.features[i].geometry.coordinates.length; k++){
-					// console.log(data.features[i].geometry.coordinates[k]);
-					// long = data.features[i].geometry.coordinates[0];
-					// latt = data.features[i].geometry.coordinates[1];
-					// console.log('latitude ' + latt + ' longditude ' + long);
-				// }
-				var obj = {};
-				// stores coordinates in an object
-				obj.lat = JSON.parse(data.features[i].geometry.coordinates[1]);
-				obj.lng = JSON.parse(data.features[i].geometry.coordinates[0]);
+   var script = document.createElement('script');
+    script.src='https://maps.googleapis.com/maps/api/js?key='+ myKey[0].key;
 
-				// Pushes long and lattitude into the obj object
-				markers.push(obj);
-			}
-			console.log(markers);
-				initMap(markers);
-		} , 
-		error : function(){
-			alert('You done goofed somewhere in your code');
-		}
+    initMap();
 
-	});
-	// Ajax ends
 
-	//dynamically creating script tag and appending to the html body including the apikey
-	var script = document.createElement('script');
-	script.src = 'https://maps.googleapis.com/maps/api/js?key='+ myKey ;
-	document.getElementsByTagName('body')[0].appendChild(script);
 
-	// Adds map markers based on cooridinates
-	function initMap(allMarkers) {
-		console.log(allMarkers);
-		var marker = [];
-		// The center point of the map, Wellington
-		var Wellington = {lat: -41.2865, lng: 174.7762};
-		// Creating the new map object
-		var map = new google.maps.Map(
-			document.getElementById('map'), {zoom: 6, center: Wellington});
-		// var i;
-		var myIcon = {
-			url : 'http://maps.google.com/mapfiles/kml/shapes/sailing.png' , 
-			scaledSize: new google.maps.Size(50, 50)
-		};
+	// $.ajax({
+	//   url : 'https://api.geonet.org.nz/intensity?type=reported',
+	// 	type :'GET',
+	// 	dataType :'json',
+	// 	success:function(data){
+  //     console.log(data);
+  //
+  //     for (var i = 0; i < data.features.length; i++) {
+  //       for (var j = 0; j < data.features[i].geometry.coordinates.length; j++) {
+  //           if (j == 0) {
+  //              lng = data.features[i].geometry.coordinates[j];
+  //             console.log(lng);
+  //           } else if (j == 1) {
+  //              lat = data.features[i].geometry.coordinates[j];
+  //             console.log(lat);
+  //           }
+  //
+  //   			}
+  //
+  //         initMap();
+  //
+  //
+  //
+  //       console.log(i);
+  //     }
+  //
+  //
+  //
+	// 	}, error:function(){
+	// 		console.log('error');
+	// 	}
+  //
+	// });//ajax
 
-		var k;
-		for(k = 0; k < allMarkers.length; k++){
-			var latLng = {lat:allMarkers[k].lat , lng:allMarkers[k].lng};
-			
-			marker = new google.maps.Marker({
-				position: latLng,
-				map: map
-			});
-		}
-	}
-}); // Document ready ends
+  // Initialize and add the map
+  function initMap() {
+    // The location of Uluru
+    var uluru = {lat: lat, lng: lng};
+    console.log(uluru);
+    // The map, centered at Uluru
+    var map = new google.maps.Map(
+        document.getElementById('map'), {zoom: 4, center: uluru});
+    // The marker, positioned at Uluru
+    var marker = new google.maps.Marker({position: uluru, map: map});
 
+  }
+}); //document ready
+
+
+
+// <script async defer
+// src="https://maps.googleapis.com/maps/api/js?key=" + myKey&callback=initMap">
+// </script>
